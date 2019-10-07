e7b6dfa76522c2bb422710f1d64eca8f1a563591
JJavier98 <jota.jota.98@hotmail.com>
2019-10-07 12:27:50 +0200

    Prueba de tests DER, adición de Travis closes #6

---
+++ test/test_DER.js
@@ -0,0 +1,28 @@
+var DER = require('../src/DER.js');
+var assert = require('assert');
+
+describe('Clase DER', function() {
+    // Define DER
+    var d = new DER('DER #1', 200, 300, undefined)
+    var d1 = new DER('DER #1', 200, 300, 'cargador coche eléctrico')
+
+    it('should assign correct value to name attribute', function() {
+        assert.equal(d.name, 'DER #1')
+    });
+    
+    it('should assign correct value to lalitud attribute', function() {
+        assert.equal(d.latitud, 200)
+    });
+    
+    it('should assign correct value to longitud attribute', function() {
+        assert.equal(d.longitud, 300)
+    });
+    
+    it('should assign correct value to undefined type attribute', function() {
+        assert.equal(d.type, 'instalación fotovoltaica')
+    });
+    
+    it('should assign correct value to type attribute', function() {
+        assert.equal(d1.type, 'cargador coche eléctrico')
+    });
+});
