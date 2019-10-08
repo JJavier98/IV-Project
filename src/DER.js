c02cde2d81d0d095f5e1ea4e2d5bd5ea118e8d8a
JJavier98 <jota.jota.98@hotmail.com>
2019-10-08 20:14:35 +0200

    jsdoc added to devDeppendences. CHange to work on PC

--- src/DER.js
+++ src/DER.js
@@ -1,4 +1,12 @@
 class DER {
+    /**
+     * Constructor de la clase
+     * @constructor
+     * @param {String} name - Nombre del dispositivo DER
+     * @param {float} latitud - Valor de la latitud en la que se encuentra el DER
+     * @param {float} longitud - Valor de la longitud en la que se encuentra el DER
+     * @param {String} type - Describe qué funcionalidad tiene el DER
+     */
     constructor(name, latitud, longitud, type = 'instalación fotovoltaica') {
         this.name = name            // Clave primaria (string)
         this.latitud = latitud      // Coordenada 1 (float)
--- src/index.js
+++ src/index.js
@@ -2,38 +2,3 @@ var Miembro = require('./miembro.js')
 var Gestor = require('./gestor.js')
 var Comunidad = require('./comunidad.js')
 var DER = require('./DER.js')
-
-var d = new DER('DER #1', 200, 300, undefined)
-var c = new Comunidad('Com #1', 'Comunidad de paneles solares', 100, 350, undefined)
-var g = new Gestor('12345678-J', 'JJ', undefined, undefined, c)
-c.gestor = g
-var m = new Miembro('77766199-W', 'Jota', undefined, undefined, c)
-
-// Gestor
-console.log(g.dni)
-console.log(g.name)
-console.log(g.last_name)
-console.log(g.id_DER)
-console.log(g.id_community)
-console.log(g.gestor)
-
-// Comunidad
-console.log(c.name)
-console.log(c.gestor)
-console.log(c.longitud)
-console.log(c.latitud)
-console.log(c.miembros)
-
-// DER
-console.log(d.name)
-console.log(d.latitud)
-console.log(d.longitud)
-console.log(d.type)
-
-// Miembro
-console.log(m.dni)
-console.log(m.name)
-console.log(m.last_name)
-console.log(m.id_DER)
-console.log(m.id_community)
-console.log(m.gestor)
