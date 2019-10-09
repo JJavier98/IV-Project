f1baee496e7ceebbf1c57d033bd61b0beb236ef8
JJavier98 <jota.jota.98@hotmail.com>
2019-10-09 23:23:03 +0200

    change to work on laptop

--- src/index.js
+++ src/index.js
@@ -24,19 +24,10 @@ app.listen(app.get('port'), () => {
     console.log('Server on port ', app.get('port'));
 });
 
-var nano = require('nano')('http://admin:admin@localhost:5984');
-nano.db.create('books');
+var nano = require('nano')('http://localhost:5984');
+//nano.db.create('books'); 
 var books = nano.db.use('books');
 
-//Insert a book document in the books database
-books.insert({name: 'The Art of war'}, null, function(err, body) {
-  if (!err){
-    console.log(body);
-  }
-});
-
-//Get a list of all books
-books.list(function(err, body){
-  console.log(body);
-});
-
+books.get('book1').then((body) => {
+  console.log(body.name)
+})
