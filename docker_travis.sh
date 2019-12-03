d1775419f56d740d5dfa059f47e920176f6e8777
JJavier98 <jota.jota.98@hotmail.com>
2019-12-03 02:21:10 +0100

    :(

--- docker_travis.sh
+++ docker_travis.sh
@@ -2,5 +2,5 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t ecm .
-docker tag ecm jjavier98/ecm
+docker tag ecm jjavier98/ecm:latest
 docker push jjavier98/ecm
